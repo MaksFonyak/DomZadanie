e3a3da085ae8d8120d14e82cd0940db08224fd4e
Maks <fonyak_maks@mail.ru>
2022-11-30 12:28:49 +0500

    все еще думаю над решением задачи 19

--- Lesson003/zadacha19/Program.cs
+++ Lesson003/zadacha19/Program.cs
@@ -5,3 +5,11 @@
 // 12821 -> да
 
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[4];
+if ((arr[0] == arr[4]) && (arr[1] == arr[3]))
+{Console.WriteLine($"Число {num} - полиндром: ");} 
+else
+Console.WriteLine($"Число {num} -не полиндром: ");
