5eb2e59eb45edd5d86b4f4eae8717d31b1b055e8
Maks <fonyak_maks@mail.ru>
2022-12-06 16:41:31 +0500

    решил задиние 29

--- Lesson004/zadacha27/Program.cs
+++ Lesson004/zadacha27/Program.cs
@@ -5,3 +5,38 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+// int Sum (int num)
+// {
+//     int i = 0;
+//     while(i < num)
+//     {
+//         num = num / 10;
+//         i++;
+//     }
+//     return i;
+// }
+// Console.WriteLine("Input number");
+// int x = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int x = 788;
+int x1 = 0;
+int x2 = 0;
+if (x > 0 && x < 100) 
+{
+    1 = x / 10;
+    Console.WriteLine(x1);
+}
+
+x2 = x % 10;
+Console.WriteLine(x2);
+sum = x1 + x2;
+Console.WriteLine(sum);
+
+
+
+// Console.WriteLine("Input number");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int answer = Sum(x);
+// Console.WriteLine(answer);
+
--- Lesson004/zadacha29/Program.cs
+++ Lesson004/zadacha29/Program.cs
@@ -3,3 +3,19 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+int[] PrintArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = rnd.Next(0, 99);
+    }
+    return array;
+}
+int len = 8;
+int[] array = PrintArray(len);
+for (int i = 0; i < len; i++)
+{
+    Console.Write($"{array[i]} ");
+}
