185876909b69002a797f95a6525db2d92f683b60
Maks <fonyak_maks@mail.ru>
2022-12-06 14:24:31 +0500

    решил задиние 25

--- Lesson004/zadacha25/Program.cs
+++ Lesson004/zadacha25/Program.cs
@@ -3,3 +3,20 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+int Expon(int a, int  b)
+{
+    int count = 1;
+    int expon = a;
+    while(count < b)
+    {
+        expon = expon * a;
+        count++;
+    }
+    return expon;
+}
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Expon(a, b));
