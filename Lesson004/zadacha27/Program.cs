f99d167f1c69bb661693f3e35f43ddf23381f841
Maks <fonyak_maks@mail.ru>
2022-12-08 15:14:04 +0500

    решил задиние 27

--- Lesson004/zadacha27/Program.cs
+++ Lesson004/zadacha27/Program.cs
@@ -1,42 +1,32 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// 452 -> 11
+// // 452 -> 11
 
-// 82 -> 10
+// // 82 -> 10
 
-// 9012 -> 12
+// // 9012 -> 12
 
-// int Sum (int num)
-// {
-//     int i = 0;
-//     while(i < num)
-//     {
-//         num = num / 10;
-//         i++;
-//     }
-//     return i;
-// }
-// Console.WriteLine("Input number");
-// int x = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int x = 788;
-int x1 = 0;
-int x2 = 0;
-if (x > 0 && x < 100) 
+
+
+int sumNumbers(int num)
 {
-    1 = x / 10;
-    Console.WriteLine(x1);
+    int result = 0;
+    num = Math.Abs(num);
+    if (num < 10)
+        result = num;
+    else
+    {
+        int ind = num;
+        while(ind > 0)
+        {
+            result = result + ind % 10;
+            ind = ind /10;
+        }
+    }
+    return result;
 }
 
-x2 = x % 10;
-Console.WriteLine(x2);
-sum = x1 + x2;
-Console.WriteLine(sum);
-
-
-
-// Console.WriteLine("Input number");
-// int x = Convert.ToInt32(Console.ReadLine());
-// int answer = Sum(x);
-// Console.WriteLine(answer);
-
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {num} равна {sumNumbers(num)}");
